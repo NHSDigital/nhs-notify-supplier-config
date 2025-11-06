b89aaef0e02a757e2092277eabe46b9752e11475
Mike Houston <michael.houston4@nhs.net>
2025-11-06 10:58:43 +0000

    Refactor letter variant schemas to use lowercase status and update related event handling

--- packages/events/src/events/letter-variant-events.ts
+++ packages/events/src/events/letter-variant-events.ts
@@ -5,9 +5,13 @@ import {
   $LetterVariantStatus,
 } from "@nhsdigital/nhs-notify-event-schemas-supplier-config/src/domain/letter-variant";
 
-const eventStatuses = $LetterVariantStatus.options.filter(
+const variantStatuses = $LetterVariantStatus.options.filter(
   (status) => status !== "DRAFT",
 );
+
+const variantStatusRegex = variantStatuses
+  .map((status) => status.toLowerCase())
+  .join("|");
 
 /**
  * A generic schema for parsing any letter status change event
@@ -16,18 +20,17 @@ export const $LetterVariantEvent = $EnvelopeProfile
   .safeExtend({
     type: z
       .enum(
-        eventStatuses.map(
+        variantStatuses.map(
           (status) =>
-            `uk.nhs.notify.supplier-config.letter-variant.${status}.v1`,
+            `uk.nhs.notify.supplier-config.letter-variant.${status.toLowerCase()}.v1`,
         ),
       )
       .meta({
         title: `Letter event type`,
         description: "Event type using reverse-DNS style",
         examples: [
-          "uk.nhs.notify.supplier-config.letter-variant.DRAFT.v1",
-          "uk.nhs.notify.supplier-config.letter-variant.PUBLISHED.v1",
-          "uk.nhs.notify.supplier-config.letter-variant.DISABLED.v1",
+          "uk.nhs.notify.supplier-config.letter-variant.published.v1",
+          "uk.nhs.notify.supplier-config.letter-variant.disabled.v1",
         ],
       }),
 
@@ -35,7 +38,7 @@ export const $LetterVariantEvent = $EnvelopeProfile
       .string()
       .regex(
         new RegExp(
-          `^https:\\/\\/notify\\.nhs\\.uk\\/cloudevents\\/schemas\\/supplier-config\\/letter-variant\\.(?<status>${eventStatuses.join("|")})\\.1\\.\\d+\\.\\d+\\.schema.json$`,
+          `^https:\\/\\/notify\\.nhs\\.uk\\/cloudevents\\/schemas\\/supplier-config\\/letter-variant\\.(?<status>${variantStatusRegex})\\.1\\.\\d+\\.\\d+\\.schema.json$`,
         ),
       )
       .meta({
@@ -44,7 +47,8 @@ export const $LetterVariantEvent = $EnvelopeProfile
 
 Data schema version must match the major version indicated by the type`,
         examples: [
-          "https://notify.nhs.uk/cloudevents/schemas/supplier-config.letter-variant.PUBLISHED.1.0.0.schema.json",
+          "https://notify.nhs.uk/cloudevents/schemas/supplier-config.letter-variant.published.1.0.0.schema.json",
+          "https://notify.nhs.uk/cloudevents/schemas/supplier-config.letter-variant.disabled.1.0.0.schema.json",
         ],
       }),
 
@@ -94,24 +98,23 @@ export type LetterVariantEvent = z.infer<typeof $LetterVariantEvent>;
  * Specialise the generic event schema for a single status
  * @param status
  */
-const specialiseLetterVariantEvent = (status: (typeof eventStatuses)[0]) =>
-  $LetterVariantEvent
+const specialiseLetterVariantEvent = (status: (typeof variantStatuses)[0]) => {
+  const lcStatus = status.toLowerCase();
+  const eventType = `uk.nhs.notify.supplier-config.letter-variant.${lcStatus}.v1`;
+
+  return $LetterVariantEvent
     .safeExtend({
-      type: z
-        .literal(`uk.nhs.notify.supplier-config.letter-variant.${status}.v1`)
-        .meta({
-          title: `LetterVariant ${status} event type`,
-          description: "Event type using reverse-DNS style",
-          examples: [
-            `uk.nhs.notify.supplier-config.letter-variant.${status}.v1`,
-          ],
-        }),
+      type: z.literal(eventType).meta({
+        title: `LetterVariant ${status} event type`,
+        description: "Event type using reverse-DNS style",
+        examples: [eventType],
+      }),
 
       dataschema: z
         .string()
         .regex(
           new RegExp(
-            `^https:\\/\\/notify\\.nhs\\.uk\\/cloudevents\\/schemas\\/supplier-config\\/letter-variant.${status}.1\\.\\d+\\.\\d+\\.schema.json$`,
+            `^https:\\/\\/notify\\.nhs\\.uk\\/cloudevents\\/schemas\\/supplier-config\\/letter-variant.${lcStatus}.1\\.\\d+\\.\\d+\\.schema.json$`,
           ),
         )
         .meta({
@@ -120,7 +123,7 @@ const specialiseLetterVariantEvent = (status: (typeof eventStatuses)[0]) =>
 
 Data schema version must match the major version indicated by the type`,
           examples: [
-            `https://notify.nhs.uk/cloudevents/schemas/supplier-config/letter-variant.${status}.1.0.0.schema.json`,
+            `https://notify.nhs.uk/cloudevents/schemas/supplier-config/letter-variant.${lcStatus}.1.0.0.schema.json`,
           ],
         }),
 
@@ -136,13 +139,14 @@ For this event the status is always \`${status}\``,
         }),
     })
     .meta({
-      title: `letter.${status} Event`,
-      description: `Event schema for letter status change to ${status}`,
+      title: `letter-variant.${lcStatus} Event`,
+      description: `Event schema for letter variant change to ${status}`,
     });
+};
 
 export const letterVariantEvents = Object.fromEntries(
-  eventStatuses.map((status) => [
-    `letter-variant.${status}`,
+  variantStatuses.map((status) => [
+    `letter-variant.${status.toLowerCase()}`,
     specialiseLetterVariantEvent(status),
   ]),
 );
